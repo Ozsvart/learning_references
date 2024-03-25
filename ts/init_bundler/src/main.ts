848cb6bec03fdba56b912afedcb6474d018e3b50
Ozsvart <v.ozsvart@gmail.com>
2024-03-25 15:46:31 +0100

    Object  Basics, Types, Interfaces

--- ts/init_bundler/src/main.ts
+++ ts/init_bundler/src/main.ts
@@ -27,3 +27,42 @@ g = "sdf"
 const h = JSON.parse("sdfsdf") // the type is any
 const i = fetch("sdfsdf").then(res => res.json()).then(data => { data }) // data also has the type any
 // Using any cuts down all the benefits of using TS, so you should avoid it.
+
+// Object Basics
+const personExample: {name: string; age: number; isProgrammer?: boolean} = { name: "Kyle", age: 28} // question mark syntax makes the property optional
+// The properties can be separated by semi-colon, colon or a new line.
+
+// Types
+
+type Person /* camelcase! */ = {name: string; age: number; isProgrammer?: boolean; friends: string[]; address: { street: string}}
+const person: Person = {
+    name: "Kyle",
+    age: 28,
+    friends: [],
+    address: {
+        street: "Main St."
+    }
+}
+
+const person2: Person = {
+    name: "Sally",
+    age: 22,
+    friends: [],
+    address: {
+        street: "Main St."
+    }
+}
+
+// Type Person can also be assigned a number, the type can be changed to anything. It is more flexible than interfaces.
+
+// Interfaces
+// Interface work with objects only.
+
+interface PersonInterface {
+    name: "Kyle",
+    age: 28,
+    friends: [],
+    address: {
+        street: "Main St."
+    }
+}
