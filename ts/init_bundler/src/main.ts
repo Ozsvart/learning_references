bf9673c7745c9b6aa4dc5d11afb051ba48629911
Ozsvart <v.ozsvart@gmail.com>
2024-03-25 15:17:15 +0100

    Array Type, Any Type

--- ts/init_bundler/src/main.ts
+++ ts/init_bundler/src/main.ts
@@ -12,4 +12,18 @@ const c = null
 let d: null = null
 d = undefined // it throws an error
 
+// Array Type
+const e: number[] = [1, 2, 3]
+//
+const f: [1, 2, 3] = [1, 2, 3]
+f.push(4) // throws error, because in this case nothing can be modified in the array
 
+// Any Type
+let g: any = 4
+
+g = 3
+g = "sdf"
+
+const h = JSON.parse("sdfsdf") // the type is any
+const i = fetch("sdfsdf").then(res => res.json()).then(data => { data }) // data also has the type any
+// Using any cuts down all the benefits of using TS, so you should avoid it.
